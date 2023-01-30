30627a2c6562f809133fcf8139419b058224d1fb
Mark de Vries <mark@lamalama.nl>
2023-01-30 11:50:32 +0100

    Remove API url from config

--- config/productive.php
+++ config/productive.php
@@ -1,7 +1,6 @@
 <?php
 
 return [
-    'api_url' => env('PRODUCTIVE_API_URL', 'https://api.productive.io/api/v2'),
     'auth_token' => env('PRODUCTIVE_AUTH_TOKEN'),
     'organization_id' => env('PRODUCTIVE_ORGANIZATION_ID', 15413),
 ];
